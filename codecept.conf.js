aa6c13fdc06d035ceb20905379d050859b0f24ae
吉野和宏 <moguai@sky.hi-ho.ne.jp>
2021-10-06 21:35:39 +0900

    URL取得のための正規表現を修正

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { setHeadlessWhen } = require('@codeceptjs/configure');
 
 // turn on headless mode when running with HEADLESS=true environment variable
@@ -10,7 +9,7 @@ exports.config = {
   output: './output',
   helpers: {
     WebDriver: {
-      url: 'http://localhost',
+      url: 'https://hotel.testplanisphere.dev/ja/',
       browser: 'chrome'
     },
     "ChaiWrapper" : {
@@ -42,49 +41,4 @@ exports.config = {
       enabled: true
     }
   }
-=======
-const { setHeadlessWhen } = require('@codeceptjs/configure');
-
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-
-exports.config = {
-  tests: 'mail_test/*_test.js',
-  output: './output',
-  helpers: {
-    WebDriver: {
-      url: 'http://localhost',
-      browser: 'chrome'
-    },
-    "ChaiWrapper" : {
-      "require": "codeceptjs-chai"
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'parser',
-  translation: 'ja-JP',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true
-    },
-    tryTo: {
-      enabled: true
-    },
-    screenshotOnFail: {
-      enabled: true
-    },
-    "ChaiWrapper" : {
-      "require": "codeceptjs-chai"
-    },
-    allure: {
-      enabled: true
-    }
-  }
->>>>>>> 017d8eba44354dc97a9e7a23701adc904b35d092
 }
--- ext_program.js
+++ ext_program.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-
-//var body = null;
 module.exports.runSendMail = function(address,rank,subject,simei,id,url,callback_func) {
     var nodemailer = require('nodemailer');
     var smtpConfig = {
@@ -35,43 +31,3 @@ module.exports.runSendMail = function(address,rank,subject,simei,id,url,callback
    });
     return body;
 }
-
-=======
-
-
-//var body = null;
-module.exports.runSendMail = function(address,rank,subject,simei,id,url,callback_func) {
-    var nodemailer = require('nodemailer');
-    var smtpConfig = {
-        port: 465,
-        host: 'smtp.gmail.com',
-        secureConnection: true,
-        auth: {
-            user: 'ココにgmailのID',
-            pass: 'ココにgmailのパスワード'
-        }
-    };
-    const transporter = nodemailer.createTransport(smtpConfig);
-    var body01 = '様。\r\n';
-    var body02 = 'この度は当ホテルに会員登録いただき、誠にありがとうございました。\r\n';
-    var body03 = '登録手続きが完了いたしましたので、ご連絡申し上げます。\r\n';
-    var body04 = 'ユーザーID：';
-    var body05 = 'パスワードは登録時に設定頂いたものをお使い頂けます。\r\n';
-    var body06 = 'ご登録頂いた内容につきましては、以下のアドレスにてご確認頂けます。\r\n';
-    var body07 = 'それでは、ご利用を心からお待ち申し上げております。\r\nhttps://hotel.testplanisphere.dev/ja/index.html';
-
-    var body = rank + '\r\n' + simei + body01 + body02 + body03 + body04 + id + '\r\n' + body05 + body06 + url + '\r\n' + body07;
-
-   transporter.sendMail({
-       from: '送信元アドレス',
-       to: 'テストに使うgmailアドレス',
-       subject: subject,
-       text: body
-   }, function(e, res){
-       console.log(e ? e.message : res.message);
-       smtp.close();
-   });
-    return body;
-}
-
->>>>>>> 017d8eba44354dc97a9e7a23701adc904b35d092
