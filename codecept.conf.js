017d8eba44354dc97a9e7a23701adc904b35d092
吉野和宏 <48610095+KazuhiroYoshino@users.noreply.github.com>
2021-10-06 10:54:54 +0900

    初回コミット

---
+++ codecept.conf.js
@@ -0,0 +1,44 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: 'mail_test/*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost',
+      browser: 'chrome'
+    },
+    "ChaiWrapper" : {
+      "require": "codeceptjs-chai"
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'parser',
+  translation: 'ja-JP',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    },
+    "ChaiWrapper" : {
+      "require": "codeceptjs-chai"
+    },
+    allure: {
+      enabled: true
+    }
+  }
+}
